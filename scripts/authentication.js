79d5015b4e836baa66cb1285df6dcd29af2f2815
Annyn Matheson <annynm@gmail.com>
2024-03-18 16:33:28 -0700

    Add FireStore and update name for nav and in other locations

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -6,7 +6,23 @@ var uiConfig = {
         // User successfully signed in.
         // Return type determines whether we continue the redirect automatically
         // or whether we leave that to developer to handle.
-        return true;
+        var user = authResult.user;                            // get the user object from the Firebase authentication database
+         if (authResult.additionalUserInfo.isNewUser) {         //if new user
+             db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
+                    name: user.displayName,                    //"users" collection
+                    email: user.email,                         //with authenticated user's ID (user.uid)
+                    country: "",                      //optional default profile info      
+										city: ""                          //optional default profile info
+             }).then(function () {
+                    console.log("New user added to firestore");
+                    window.location.assign("main.html");       //re-direct to main.html after signup
+             }).catch(function (error) {
+                    console.log("Error adding new user: " + error);
+             });
+         } else {
+             return true;
+         }
+             return false;
       },
       uiShown: function() {
         // The widget is rendered.
@@ -30,17 +46,17 @@ var uiConfig = {
     tosUrl: '<your-tos-url>',
     // Privacy policy url.
     privacyPolicyUrl: '<your-privacy-policy-url>'
-  };
+};
 
-  ui.start('#firebaseui-auth-container', uiConfig)
+ui.start('#firebaseui-auth-container', uiConfig)
 
-  document.getElementById('signOutButton').addEventListener('click', function() {
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-        console.log('User signed out.');
-        window.location.href = "index.html";
-    }).catch(function(error) {
-        // An error happened.
-        console.error('Error signing out:', error);
-    });
+document.getElementById('signOutButton').addEventListener('click', function() {
+  firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      console.log('User signed out.');
+      window.location.href = "index.html";
+  }).catch(function(error) {
+      // An error happened.
+      console.error('Error signing out:', error);
+  });
 });
